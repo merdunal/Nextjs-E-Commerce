43e81271b8af4d72e668d7d3be02b02efbce0e69
Mert ÜNAL <mertunal878@gmail.com>
2024-09-30 16:55:46 +0300

    add main page image slider

---
+++ src/components/MainImageSlider.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { useEffect, useRef } from "react";
+import { Pagination } from "swiper/modules";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+
+const MainImageSlider = () => {
+  const sliderRef = useRef<any>(null);
+
+  const images = [
+    "/hippo-email-sent.png",
+    "/hippo-empty-cart.png",
+    "/nhk8jg3psng71.jpg",
+    // Add more image paths as needed
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (sliderRef.current) {
+        sliderRef.current.swiper.slideNext();
+      }
+    }, 5000); // Change slides every 5 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="relative bg-zinc-100 overflow-hidden rounded-xl h-[480px] w-[1280px]">
+      <Swiper
+        ref={sliderRef}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        slidesPerView={1}
+        loop={true} // Enable looping
+        className="h-full w-full"
+      >
+        {images.map((src, index) => (
+          <SwiperSlide key={index} className="relative h-full w-full">
+            <Image
+              src={src}
+              alt={`Slide image ${index + 1}`}
+              layout="fill" // Use layout fill for responsive resizing
+              objectFit="cover" // Crop the image to fill
+              className="rounded-xl" // Optional: Add rounded corners if desired
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      {/* Navigation Buttons */}
+      <div className="absolute z-10 inset-0 flex items-center justify-between opacity-100 transition">
+        <button
+          onClick={() => sliderRef.current.swiper.slidePrev()}
+          className="hover:bg-gray-500 text-white rounded-full p-2 m-2"
+          aria-label="Previous Slide"
+        >
+          <ChevronLeft className="h-6 w-6" />
+        </button>
+        <button
+          onClick={() => sliderRef.current.swiper.slideNext()}
+          className="hover:bg-gray-500 text-white rounded-full p-2 m-2"
+          aria-label="Next Slide"
+        >
+          <ChevronRight className="h-6 w-6" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default MainImageSlider;
