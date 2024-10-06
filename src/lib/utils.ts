8b53c1e9571796709ef5cfe45826d57dd9d3c040
Mert ÜNAL <mertunal878@gmail.com>
2024-10-06 17:23:10 +0300

    vercel deployment

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
+import { Metadata } from "next";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -23,4 +24,47 @@ export function formatPrice(
     //minimumFractionDigits: 2,
     maximumFractionDigits: 3,
   }).format(numericPrice);
+}
+
+export function constructMetadata({
+  title = 'YeşilBeyaz Aksesuar',
+  description = 'YeşilBeyaz Aksesuar',
+  image = '/thumbnail.png',
+  icons = '/favicon.ico',
+  noIndex = false,
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@YesilBeyaz',
+    },
+    icons,
+    //metadataBase: new URL('https://digitalhippo.up.railway.app'),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  }
 }
