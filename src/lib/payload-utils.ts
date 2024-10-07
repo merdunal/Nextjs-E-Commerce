56fa814c73471604eb6d38db1e0bd7b9a9d8492b
Mert ÜNAL <mertunal878@gmail.com>
2024-10-07 18:16:17 +0300

    vercel deployment

--- src/lib/payload-utils.ts
+++ src/lib/payload-utils.ts
@@ -8,7 +8,7 @@ export const getServerSideUser = async (
   const token = cookies.get("payload-token")?.value;
 
   const meRes = await fetch(
-    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`,
+    "https://yesilbeyaz-e-commerce.vercel.app//api/users/me",
     {
       headers: {
         Authorization: `JWT ${token}`,
