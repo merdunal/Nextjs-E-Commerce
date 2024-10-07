b3af63479fee9aa80a0ba428898303c2c9d38670
Mert ÜNAL <mertunal878@gmail.com>
2024-10-07 19:05:14 +0300

    vercel deployment

--- src/lib/payload-utils.ts
+++ src/lib/payload-utils.ts
@@ -17,7 +17,20 @@ export const getServerSideUser = async (
     }
   );
 
+  console.log("mRes:", meRes)
+  console.log("mRes json:", meRes.json())
+
+  if (!meRes.ok) {
+    console.error("Failed to fetch user data:", await meRes.text());
+    return { user: null };
+  }
+
   const { user } = (await meRes.json()) as { user: User | null };
+  console.log("user:", user)
 
   return { user };
+
+
+
+
 };
